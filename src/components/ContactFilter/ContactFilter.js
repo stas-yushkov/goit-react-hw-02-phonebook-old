fedc88b0967e145865b3b4a2970877b3b60bc6f0
Stanislav Yushkov <s.yushkov@cargoo.ua>
2021-09-17 13:17:35 +0300

    add debouncedOnFilter

--- src/components/ContactFilter/ContactFilter.js
+++ src/components/ContactFilter/ContactFilter.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import debounce from 'lodash.debounce'
 
 import { StyledContactFilter } from './StyledComponrnts'
 
@@ -7,9 +8,12 @@ class ContactFilter extends Component{
     filter: ''
   }
 
+  debouncedOnFilter = debounce(this.props.onFilter, 300);
+
   onFilterChange = (e) => {
     this.setState({filter: e.target.value});
-    this.props.onFilter(e.target.value);
+    // debounce(() => this.props.onFilter(e.target.value), 300);
+    this.debouncedOnFilter(e.target.value);
   }
 
   render() {
